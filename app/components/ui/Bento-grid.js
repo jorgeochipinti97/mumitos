6356e81beea25cb143cd002f52c9a55d7f4b0bb3
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-01-17 14:22:48 -0300

    2

--- app/components/ui/Bento-grid.js
+++ app/components/ui/Bento-grid.js
@@ -19,7 +19,7 @@ export const BentoGridItem = ({ className, title, href, header }) => {
     <Link href={href}>
       <div
         className={cn(
-          "h-auto cursor-pointer row-span-1 rounded-xl  group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  bg-white border border-transparent justify-between flex flex-col space-y-4",
+          "h-full cursor-pointer row-span-1 rounded-xl  group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  bg-white border border-transparent justify-between flex flex-col space-y-4",
           className
         )}
       >
